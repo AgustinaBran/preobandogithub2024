848c261d93d1782d2edbaaacdc58398054b19366
AgusBrandemann <agusbrandemann@hotmail.com>
2024-06-02 13:13:07 -0300

    inicio

--- administrare.js
+++ administrare.js
@@ -16,3 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
+
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  var email = document.getElementById("disabledTextInput").value;
+  var password = document.getElementById("disabledTextInput").value;
+
+  if (!isValidEmail(email)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      event.preventDefault(); 
+  } else if (password.length < 8) {
+      alert("La contraseña debe tener al menos 8 caracteres.");
+      event.preventDefault(); 
+  }
+});
+
+function isValidEmail(email) {
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
