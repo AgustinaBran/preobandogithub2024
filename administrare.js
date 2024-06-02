18fb6de2de49f23ded917fbb414ef87abe9befe6
AgusBrandemann <agusbrandemann@hotmail.com>
2024-06-02 11:30:40 -0300

    validacion form

--- administrare.js
+++ administrare.js
@@ -1,9 +1,18 @@
 
-const button= document.querySelector("form-control");
 
-function action(){
-    alert("Hello");
 
-}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('registroForm').addEventListener('submit', function(event) {
+      var nombre = document.getElementById('disabledTextInput').value.trim();
+      var apellido = document.getElementById('disabledTextInput').value.trim();
+      var email = document.getElementById('disabledTextInput').value.trim();
+      var nacimiento = document.getElementById('nacimientoInput').value.trim();
+      var pais = document.getElementById('disabledSelect').value;
+      var disabledFieldsetCheck = document.getElementById('disabledFieldsetCheck').checked;
 
-button.addEventListener("click",action);
+      if (nombre === '' || apellido === '' || email === '' || nacimiento === '' || pais === '' || !terminosCheckbox) {
+          alert('Por favor, completa todos los campos y acepta los términos y condiciones.');
+         
+      }
+  });
+});
